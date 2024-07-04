7b29ae38b6ae058f15d5a8f72d2f0431fe7d6b50
devesh tripathi <deveshtrip10@gmail.com>
2024-07-05 00:00:00 +0530

    todays_progress

--- Timer/index.js
+++ Timer/index.js
@@ -1,14 +1,12 @@
 let a="hello";
-let b=10;
-let [count, setCount]=usestate(0);
-setCount=count+a+b;
-console.log(setCount)
-document.getElementById("hello").innerText=a;
 
-inputs[0].value=10;
-inputs[1]=2;
-inputs[2]=3;
-inputs[3]=4;
+document.getElementById("hello").innerHTML=a;
+let input =document.querySelectorAll("input")
+
+input[0].value=12;
+input[1].value=2;
+input[2].value=3;
+input[3].value=4;
 
 
 
