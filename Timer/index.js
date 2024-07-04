892c72745115c30506a808fac62a3858b2f2c6d2
devesh tripathi <deveshtrip10@gmail.com>
2024-07-04 23:46:34 +0530

    edit

--- Timer/index.js
+++ Timer/index.js
@@ -1,5 +1,15 @@
-let a=5;
+let a="hello";
 let b=10;
 let [count, setCount]=usestate(0);
 setCount=count+a+b;
 console.log(setCount)
+document.getElementById("hello").innerText=a;
+
+inputs[0].value=10;
+inputs[1]=2;
+inputs[2]=3;
+inputs[3]=4;
+
+
+
+
