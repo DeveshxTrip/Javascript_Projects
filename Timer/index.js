9d63c73a42752b61e23dab845d2ac10e9cec96d3
devesh tripathi <deveshtrip10@gmail.com>
2024-07-05 00:02:23 +0530

    date edited

--- Timer/index.js
+++ Timer/index.js
@@ -3,7 +3,7 @@ let a="hello";
 document.getElementById("hello").innerHTML=a;
 let input =document.querySelectorAll("input")
 
-input[0].value=12;
+input[0].value=new Date();
 input[1].value=2;
 input[2].value=3;
 input[3].value=4;
